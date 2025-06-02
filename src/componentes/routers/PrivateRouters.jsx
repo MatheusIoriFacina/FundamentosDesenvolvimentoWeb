b08c77d5d0320a1b34f33013dd2bae4b03ca03c0
MatheusIoriFacina <facinamatheus12@gmail.com>
2025-06-02 15:28:01 -0400

    Delete src/componentes directory

--- src/componentes/routers/PrivateRouters.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
-
-export default function PrivateRoute({ children }) {
-  const { usuario } = useAuth();
-
-  return usuario ? children : <Navigate to="/login" />;
-}
